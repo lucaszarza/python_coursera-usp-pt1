e7c5d8249547f6ae591936b3863903683374c3c8
lucaszarza <lucas-lsz@hotmail.com>
2021-09-02 00:12:31 -0300

    refactoring bhaskara exercise

--- sem_3/1.sem3_study-bhaskara.py
+++ sem_3/1.sem3_study-bhaskara.py
@@ -1,15 +1,17 @@
 import math
 
 print("** Bem-vindo à calculadora de Bhaskara! **")
-a = int(input("Digite o valor de a: "))
-b = int(input("Digite o valor de b: "))
-c = int(input("Digite o valor de c: "))
+a = float(input("Digite o valor de a: "))
+b = float(input("Digite o valor de b: "))
+c = float(input("Digite o valor de c: "))
 
-delta = (b ** 2) - (4 * a * c)
+delta = b ** 2 - 4 * a * c
 
-print(delta)
-x1 = (- (b) + math.sqrt(delta)) / 2 * a
-x2 = (- (b) - math.sqrt(delta)) / 2 * a
-
-print(f'O valor de X1 é {x1}')
-print(f'O valor de X2 é {x2}')
+if delta == 0:
+    raiz1 = (- b + math.sqrt(delta)) / (2 * a)
+    print(f'O valor único da raiz é {raiz1}')
+else:
+    raiz1 = (- b + math.sqrt(delta)) / (2 * a)
+    raiz2 = (- b - math.sqrt(delta)) / (2 * a)
+    print(f'O valor da primeira raiz é {raiz1}')
+    print(f'O valor da segunda raiz é {raiz2}')
