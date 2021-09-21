8eb95cd9f45907c2c6843dbdb54a39a950d479ec
Lucas Zarza <lucas.zarza@doc88.com.br>
2021-09-21 20:42:53 -0300

    extra exercises

---
+++ sem_4/9.sem4_extra1_prime_numbers.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+def main():
+	num = int(input("Digíte seu número: "))
+	
+	if num % num == 0 and num % 1 == 0:
+		if num == 1:
+			print("não primo")
+		elif num == 2 or num == 3:
+			print("primo")
+		else:
+			if num % 2 == 0 or num % 3 == 0:
+				print("não primo")
+			else:
+				print("primo")
+main()
+ 
