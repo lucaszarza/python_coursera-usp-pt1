85c48e5ff47e43fa41e1a884000897f00e707de6
Lucas Zarza <lucas.zarza@doc88.com.br>
2021-09-27 22:10:04 -0300

    making extra exercise from video

---
+++ sem_5/2.sem5_study_binomial_coefficient.py
@@ -0,0 +1,29 @@
+def factorial(num):	
+	fat = 1	
+	
+	if (num == 0):
+		return 1
+	elif (num < 0):
+		return error
+	else:
+		while (num > 0):
+			fat = fat * num
+			num = num - 1
+		
+		return fat
+
+def binomial_coefficient(k, n):
+	result = (factorial(k)/(factorial(n)*factorial(k-n)))
+	
+	return result
+
+
+def main():
+	print("Bem vindo à calculadora de coeficiente binomial!")
+	n = int(input("Digite o valor de n: "))
+	k = int(input("Digite o valor de k: "))
+
+	print(binomial_coefficient(n, k))
+
+main()
+
